967123441e9394ad1a7233e586c574f58b55710a
Nick Bennett <nick@nickbenn.com>
2020-09-22 09:49:39 -0600

    Comment change.

--- src/edu/cnm/deepdive/CommandLine.java
+++ src/edu/cnm/deepdive/CommandLine.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive;
 public class CommandLine {
 
   public static void main(String[] args) {
+    // Enhanced for is less error-prone.
     for (String current : args) {
       System.out.println(current.toUpperCase());
     }
